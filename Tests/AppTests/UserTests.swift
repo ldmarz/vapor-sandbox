8cdab45e84697fd846f2f6582a5dde01a3342906
Lenin Martinez <lenin.martinez123@gmail.com>
2018-11-08 15:35:43 -0300

    Adding integrations test

---
+++ Tests/AppTests/UserTests.swift
@@ -0,0 +1,54 @@
+@testable import App
+import Vapor
+import XCTest
+import FluentPostgreSQL
+
+
+final class UserTests: XCTestCase {
+    let usersName = "Alice"
+    let usersUsername = "alicea"
+    let usersURI = "/api/users/"
+    var app: Application!
+    var conn: PostgreSQLConnection!
+    
+    
+    override func setUp() {
+        try! Application.reset()
+        app = try! Application.testable()
+        conn = try! app.newConnection(to: .psql).wait()
+    }
+    
+    override func tearDown() {
+        conn.close()
+    }
+
+    func testUsersCanBeRetrievedFromAPI() throws {
+        let user = try User.create(name: usersName, username: usersUsername, on: conn)
+        let users = try app.getResponse(to: usersURI, decodeTo: [User].self)
+
+        XCTAssertEqual(users.count, 2)
+        XCTAssertEqual(users[0].name, usersName)
+        XCTAssertEqual(users[0].username, usersUsername)
+        XCTAssertEqual(users[0].id, user.id)
+    }
+    
+    func testUserCanBeSavedWithAPI() throws {
+        let user = User(name: usersName, username: usersUsername)
+        let receivedUser = try app.getResponse(
+            to: usersURI,
+            method: .POST,
+            headers: ["Content-Type": "application/json"],
+            data: user,
+            decodeTo: User.self)
+        XCTAssertEqual(receivedUser.name, usersName)
+        XCTAssertEqual(receivedUser.username, usersUsername)
+        XCTAssertNotNil(receivedUser.id)
+        let users = try app.getResponse(
+            to: usersURI,
+            decodeTo: [User].self)
+        XCTAssertEqual(users.count, 1)
+        XCTAssertEqual(users[0].name, usersName)
+        XCTAssertEqual(users[0].username, usersUsername)
+        XCTAssertEqual(users[0].id, receivedUser.id)
+    }
+}
